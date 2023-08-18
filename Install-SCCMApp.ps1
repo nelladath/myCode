122f852061cdb0fb9dd94a6f87691648b84251d2
nelsuj01@csgicorp.com <sujin.nelladath@csgi.com>
2023-08-18 12:14:28 -0500

    SCCM Code to deploy an application

---
+++ Install-SCCMApp.ps1
@@ -0,0 +1,24 @@
+﻿Function Trigger-AppInstallation
+{
+ 
+#$Computername = $env:COMPUTERNAME
+#$AppName = "VMWare Tools v11.3.0"
+$Method = "Install"
+$Application = Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK"  
+#| Where-Object {$_.Name -like $AppName})
+ 
+$Args = @{EnforcePreference = [UINT32] 0
+#Id = "$($Application.id)"
+#IsMachineTarget = $Application.IsMachineTarget
+IsRebootIfNeeded = $False
+Priority = 'High'
+#Revision = "$($Application.Revision)"
+ }
+ 
+ 
+Invoke-CimMethod -Namespace "root\ccm\clientSDK" -ClassName CCM_Application -MethodName $Method -Arguments $Args
+ 
+}
+
+Trigger-AppInstallation
+
